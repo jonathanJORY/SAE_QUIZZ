1db6f20e39e5c5e48af1b36f3642f4f8f6c86f4a
JORY Jonathan <o22106917@info27-26.iut45.univ-orleans.fr>
2023-02-02 16:54:58 +0100

    insert

--- script.sql
+++ script.sql
@@ -13,7 +13,6 @@ CREATE TABLE REPONSE (
 );
 
 
-
 CREATE TABLE QUESTIONNAIRE (
   questionnaireID INT PRIMARY KEY,
   questionnaireName VARCHAR(255),
@@ -29,13 +28,20 @@ CREATE TABLE CONTENIR (
   FOREIGN KEY (questionID) REFERENCES questions(questionID)
 );
 
+CREATE TABLE USER (
+  userID INT PRIMARY KEY,
+  userName VARCHAR(255),
+  email VARCHAR(255),
+  Password VARCHAR(255)
+);
+
 CREATE TABLE RESULTAT (
   resultatID INT PRIMARY KEY,
---UserID INT,
+  userID INT,
   questionnaireID INT,
   questionnaireDate DATE,
   score INT,
---FOREIGN KEY (UserID) REFERENCES Users(UserID),
+  FOREIGN KEY (userID) REFERENCES USER(userID),
   FOREIGN KEY (questionnaireID) REFERENCES QUESTIONNAIRE(questionnaireID)
 );
 
@@ -48,11 +54,29 @@ INSERT INTO QUESTIONNAIRE (questionnaireID, questionnaireName, questionnaireDesc
 (6,"Test 6","ORLEANS"),
 (7,"Test 7","ORLEANS");
 
+INSERT INTO QUESTION (questionID, questionText, questionType) VALUES 
+(1,"Qui sont les fréres de Luffy ?","checkbox"),
+(2,"Le quel est mort à Marine Ford ?","radio"),
+(3,"Quel est votre personnage favoris ?","text"),
+(4,"Quel Shichibukai est le rival de Shanks ?","dropdown");
+
+INSERT INTO REPONSE (reponseID, reponseText, questionID, correct) VALUES
+(1,"Ace", 1, true),
+(2,"Garp", 1, false),
+(3,"Shanks", 1, false),
+(4,"Sabo", 1, true),
+(5,"Ace", 2, true),
+(6,"Garp", 2, false),
+(7,"Shanks", 2, false),
+(8,"Sabo", 2, false),
+(9,"Baggy Le Clown", 4, false),
+(10,"Crocodile", 4, false),
+(11,"Dracule Mihawk", 4, true),
+(12,"Marshal D Teach", 4, false),
+(13,"Boa Hancock", 4, false);
+
 INSERT INTO CONTENIR (questionnaireID, questionID, questionOrder) VALUES 
 (1,1,1),
-(2,1,1),
-(3,1,1),
-(4,1,1),
-(5,1,1),
-(6,1,1),
-(7,1,1);
+(1,2,2),
+(1,3,3),
+(1,4,4);
