4ec5098d64e2c47e77f4b0025ecf1daa3041a77f
CHARPENTIER Maxym <o22103433@info07-26.iut45.univ-orleans.fr>
2023-02-03 16:58:21 +0100

    m

--- question.php
+++ question.php
@@ -10,19 +10,28 @@
     <?php $wanted=$_GET["ID"];
         if (!empty($wanted)){
             echo "<h1>Recherche de $wanted </h1>";
-        require_once("questions.php");
+        require_once("models.php");
+        require_once("connexion.php");
         $connexion=connect_bd();
-        $sql="SELECT * from CARNET where ID=:id";
+        $sql="SELECT * from QUESTION where questionID=:id";
+        echo"ici";
         $stmt=$connexion->prepare($sql);
-        $stmt->bindParam(":id", $_GET["ID"]);
+        echo"ici";
+        $stmt->bindParam(":id", intval($_GET["ID"]), PDO::PARAM_INT);
+        echo"ici";
         $stmt->execute();
+        echo"ici";
         if (!$stmt) echo "Pb d'accès au CARNET";
         else{
-        if ($stmt->rowCount()==0) echo "Inconnu !<br/>";
-        else
-        foreach ($stmt as $row)
-        echo $row["PRENOM"]." ".$row["NOM"].
-        "né(e) le ".$row["NAISSANCE"]."<br/>";
+            if ($stmt->rowCount()==0){
+                echo "Inconnu !<br/>";
+            }
+            else{
+                echo"ici";
+                foreach ($stmt as $row)
+                echo $row["questionText"]." ".$row["questionType"]."<br/>";
+            }
+            
         }
         }
     ?>
