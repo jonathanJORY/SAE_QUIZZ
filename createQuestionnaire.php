4ee7ce7056af41da71349a6ac3308d023df50fa3
JORY Jonathan <o22106917@info07-25.iut45.univ-orleans.fr>
2023-02-03 16:39:28 +0100

    questionnaire

---
+++ createQuestionnaire.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <title>Questionnaire </title>
+</head>
+
+<div class="login-box">
+  <h2>Creer un questionnaire</h2>
+  <form id="formulaire" action="question.php"  method="post">
+    <p>
+      <label for="Titre">Titre du Questionnaire</label>
+      <input type="text" id="Titre" name="Titre" required>
+      <label for="Titre">Titre du Questionnaire</label>
+      <input type="text" id="Titre" name="Titre" required>
+    <p>
+      <input type="summit">
+    </p>
+  </form>
+</div>
+</html>
